733c591c48fae9d1bbfc48bf0d0691a3c2deb717
bobosii <day.emir@hotmail.com>
2025-03-22 17:38:26 +0300

    unnecessary changes

--- src/main/java/dev/emir/dto/CustomerDtoConverter.java
+++ src/main/java/dev/emir/dto/CustomerDtoConverter.java
@@ -3,6 +3,8 @@ package dev.emir.dto;
 import dev.emir.entities.Customer;
 import org.springframework.stereotype.Component;
 
+// I don't use this class. This class is just example for how to convert two classes manually.
+// I'm using modelMapper for convert process in this project
 @Component
 public class CustomerDtoConverter {
 
